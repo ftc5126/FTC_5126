36953e2e5bc4307da94d766df239c6017c62a1a1
emmyF18 <superemmy18@gmail.com>
2016-01-28 17:18:37 -0600

    fixed stuff that broke

--- src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DiceAutonomous.java
+++ src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DiceAutonomous.java
@@ -2,6 +2,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 
 //
 //import com.qualcomm.ftcrobotcontroller.opmodes.DiceOpModes.AutonomousRobotInfo;
@@ -11,20 +12,62 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 // * Created by Emmy on 9/24/2015.
 // */
 public class DiceAutonomous extends LinearOpMode {
-    DcMotor motorLeft = hardwareMap.dcMotor.get("motor1");
-    DcMotor motorRight = hardwareMap.dcMotor.get("motor2");
+    DcMotor motor1 = hardwareMap.dcMotor.get("motor1");
+    DcMotor motor2 = hardwareMap.dcMotor.get("motor2");
+
+
+    final double encoderConversions = .014;
 
     public void runOpMode() throws InterruptedException {
         waitForStart();
 
         while (opModeIsActive()) {
-            motorLeft.setPower(-.5);
-            motorRight.setPower(.5);
-            sleep(10000);
-            motorLeft.setPower(0);
-            motorRight.setPower(0);
+//            motorLeft.setPower(0);
+//            motorRight.setPower(0);
+//            double rightInches = convertEncoders(motorRight.getCurrentPosition());
+//            double leftInches = convertEncoders(motorRight.getCurrentPosition());
+//            //move forward to mountain
+//            moveUsingEncoders(3, .5);
+//            //turn till facing mountain
+//            turn(.8,.2);
+//            //move up mountain
+//            moveUsingEncoders(3, .5);
+            int dist = (int) (12 / encoderConversions);
+            moveUsingEncoders(dist, 0.5);
         }
-//
-//
     }
+
+    public double convertEncoders(double encoderValue) {
+        return encoderValue * encoderConversions;
+    }
+
+    //    public void sleep(long mill)
+//        {
+//        ElapsedTime eTime = new ElapsedTime();
+//        while(eTime.time() < mill)
+//        {
+//            motorLeft.setPower(0);
+//            motorRight.setPower(0);
+//        }
+//    }
+//    public void turn(double rPower, double lPower)
+//    {
+//        motorLeft.setPower(rPower);
+//        motorRight.setPower(lPower);
+//        sleep(10);
+//        motorLeft.setPower(0);
+//        motorRight.setPower(0);
+//    }
+//
+    public void moveUsingEncoders(int moveValue, double power) {
+
+        motor1.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        motor2.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        motor1.setTargetPosition(moveValue);
+        motor2.setTargetPosition(moveValue);
+        motor1.setPower(power);
+        motor2.setPower(power);
+    }
+
+
 }
